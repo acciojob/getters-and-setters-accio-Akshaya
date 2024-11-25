a094984a22c57a569c8306db741660cf36b51b97
accio-Akshaya <akshayaganesh0910@gmail.com>
2024-11-25 08:01:15 -0800

    created a submission

--- script.js
+++ script.js
@@ -1,46 +1,47 @@
 class Person {
-    constructor(name,age){
-        this.name = name;
-        this.age = age;
+    constructor(name, age) {
+        this._name = name;
+        this._age = age;
     }
-    get name(){
-        return this.name;
+
+    get name() {
+        return this._name;
     }
-    set age(newAge){
-        this.age = newAge;
-        console.log("age set");
+
+    set name(name) {
+        this._name = name;
     }
-	get age(){
-		return this.age;
-	}
+
+    get age() {
+        return this._age;
+    }
+
+    set age(age) {
+        this._age = age;
+    }
 }
 
 class Student extends Person {
-    constructor(name,age){
-        super(name, age);
-    }
-    study(){
-        return this.name + " is studying";	
+    study() {
+        console.log(`${this.name} is studying`);
     }
 }
 
 class Teacher extends Person {
-    constructor(name,age){
-        super(name, age);
-    }
-    teach(){
-        return this.name + " is teaching";
+    teach() {
+        console.log(`${this.name} is teaching`);
     }
 }
 
-const person = new Person("john",25);
-console.log(person.name);
+// Example usage:
+const person = new Person("John", 25);
+console.log(person.name); // Output: John
 
-person.age = 20;
-console.log(person.age);
+person.age = 30;
+console.log(person.age); // Output: 30
 
-const student = new Student("shakshi",22);
-console.log(student.study());
+const student = new Student("Alice", 22);
+student.study(); // Output: Alice is studying
 
-const teacher = new Teacher("Gob",34);
-console.log(teacher.teach());
+const teacher = new Teacher("Bob", 40);
+teacher.teach(); // Output: Bob is teaching
