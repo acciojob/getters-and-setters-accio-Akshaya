ac673b98251afa0a46f92c47b12061a8138f681c
accio-Akshaya <akshayaganesh0910@gmail.com>
2024-11-25 07:46:30 -0800

    created a submission

--- script.js
+++ script.js
@@ -1,58 +1,42 @@
-// Person class attached to window
-window.Person = class Person {
-    constructor(name, age) {
-        this._name = name;
-        this._age = age;
-    }
+class Person {
+	constructor(name,age){
+		this.name = name;
+		this.age = age;
+	}
+	get name(){
+		return this.name;
+	}
+	set age(newAge){
+		this.age = newAge;
+		console.log("age set");
+	}
+}
 
-    // Getter for name
-    get name() {
-        return this._name;
-    }
+class Student extends Person(){
+	constructor(name,age){
+	  super(name,age);
+	}
+	study(){
+	    return this.name + " is studying";	
+	}
+}
+class Teacher extends Person(){
+	constructor(name,age){
+	  super(name,age);
+	}
+	teach(){
+		return this.name + " is teaching";
+	}
+}
 
-    // Getter for age
-    get age() {
-        return this._age;
-    }
+const person = new Person("john",25);
+console.log(Person.name);
 
-    // Setter for age
-    set age(newAge) {
-        if (typeof newAge === 'number' && newAge >= 0) {
-            this._age = newAge;
-        } else {
-            console.error("Age must be a non-negative number");
-        }
-    }
-};
+Person.age = 20;
+console.log(Person.age);
 
-// Student class extending Person
-window.Student = class Student extends window.Person {
-    study() {
-        console.log(`${this.name} is studying`); // Fixed string interpolation
-    }
-};
+const student = new Student("shakshi",22);
+console.log(Student.study());
 
-// Teacher class extending Person
-window.Teacher = class Teacher extends window.Person {
-    teach() {
-        console.log(`${this.name} is teaching`); // Fixed string interpolation
-    }
-};
-
-// Example usage
-const person = new Person("John", 25);
-console.log(person.name); // Output: John
-
-person.age = 30;
-console.log(person.age); // Output: 30
-
-const student = new Student("Alice", 22);
-student.study(); // Output: Alice is studying
-
-const teacher = new Teacher("Bob", 40);
-teacher.teach(); // Output: Bob is teaching
-
-// Do not change the code below this line
-window.Person = Person;
-window.Student = Student;
-window.Teacher = Teacher;
+const teacher = new Teacher("Gob",34);
+console.log(teacher.teach());
