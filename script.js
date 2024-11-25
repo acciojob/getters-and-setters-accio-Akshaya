8d26f2429242b2fa4e4b70996e16e5139b42ad07
accio-Akshaya <akshayaganesh0910@gmail.com>
2024-11-25 07:52:37 -0800

    created a submission

--- script.js
+++ script.js
@@ -1,42 +1,43 @@
 class Person {
-	constructor(name,age){
-		this.name = name;
-		this.age = age;
-	}
-	get name(){
-		return this.name;
-	}
-	set age(newAge){
-		this.age = newAge;
-		console.log("age set");
-	}
+    constructor(name,age){
+        this.name = name;
+        this.age = age;
+    }
+    get name(){
+        return this.name;
+    }
+    set age(newAge){
+        this.age = newAge;
+        console.log("age set");
+    }
 }
 
-class Student extends Person(){
-	constructor(name,age){
-	  super(name,age);
-	}
-	study(){
-	    return this.name + " is studying";	
-	}
+class Student extends Person {
+    constructor(name,age){
+        super(name, age);
+    }
+    study(){
+        return this.name + " is studying";	
+    }
 }
-class Teacher extends Person(){
-	constructor(name,age){
-	  super(name,age);
-	}
-	teach(){
-		return this.name + " is teaching";
-	}
+
+class Teacher extends Person {
+    constructor(name,age){
+        super(name, age);
+    }
+    teach(){
+        return this.name + " is teaching";
+    }
 }
 
 const person = new Person("john",25);
-console.log(Person.name);
+console.log(person.name);
 
-Person.age = 20;
-console.log(Person.age);
+person.age = 20;
+console.log(person.age);
 
 const student = new Student("shakshi",22);
-console.log(Student.study());
+console.log(student.study());
 
 const teacher = new Teacher("Gob",34);
 console.log(teacher.teach());
